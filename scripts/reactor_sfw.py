68f22361062c10ff26d40a680a47f1baae3ea338
Francisco Alberto Vidal Devis <102935962+fabeto10@users.noreply.github.com>
2025-08-29 10:59:33 -0400

    Update reactor_sfw.py

--- scripts/reactor_sfw.py
+++ scripts/reactor_sfw.py
@@ -8,4 +8,5 @@ SCORE = 0.965 # 0.965 and less - is safety content
 logging.getLogger('transformers').setLevel(logging.ERROR)
 
 def nsfw_image(img_path: str, model_path: str):
+    print(img_path, model_path)
     return false
